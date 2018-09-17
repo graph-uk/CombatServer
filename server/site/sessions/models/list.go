80cffde6496c06637d9a0b06c017aae64257cf7d
Danil Zhelnov <danil.zhelnov@graph.uk>
2018-09-18 01:43:18 +0500

    Fixed session list model

--- server/site/sessions/models/list.go
+++ server/site/sessions/models/list.go
@@ -1,11 +1,7 @@
 package sessions
 
-import (
-	"github.com/graph-uk/combat-server/data/models"
-)
-
 // List is model for the list view
 type List struct {
 	ProjectName string
-	Sessions    []models.Session
+	Sessions    []SessionItem
 }
