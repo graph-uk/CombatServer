12ea9c6434123eeff719cde12ab832332dcf6df7
alexander.eliseev <alexander.eliseev@graph.uk>
2020-05-26 13:38:07 +0500

    Store win && linux binaries

---
+++ scripts/copy-client-worker-to-assets.js
@@ -0,0 +1,13 @@
+const t = new(require('./tools/tools.js'))
+
+const targetFolder = process.env['REPO']+'/src/malibu-server/assets/_/dist'
+
+t.rmdir(targetFolder)
+t.mkdir(targetFolder)
+t.mkdir(targetFolder+'/client')
+t.mkdir(targetFolder+'/worker')
+t.mkdir(targetFolder+'/malibu')
+
+t.cp(process.env['REPO']+'/src/malibu/malibu.exe', targetFolder+'/malibu/malibu.exe')
+t.cp(process.env['REPO']+'/src/malibu-client/malibu-client.exe', targetFolder+'/client/malibu-client.exe')
+t.cp(process.env['REPO']+'/src/malibu-worker/malibu-worker.exe', targetFolder+'/worker/malibu-worker.exe')
